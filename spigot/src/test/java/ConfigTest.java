55af7ce0d5bfce9a61f2f631eb25d6d4981d762e
Harveykang <46108547+Harveykang@users.noreply.github.com>
2021-01-21 21:51:39 +0000

    Delete ConfigTest.java

--- spigot/src/test/java/ConfigTest.java
+++
@@ -1,8 +0,0 @@
-public class ConfigTest {
-    public static void main(String[] args) {
-        String str = "localhost";
-        int x = str.lastIndexOf(":");
-        //System.out.println(str.substring(x+1));
-        System.out.println(str.substring(0, x));
-    }
-}
